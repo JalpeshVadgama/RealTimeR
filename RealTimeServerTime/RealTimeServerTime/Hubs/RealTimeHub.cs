9b0c8453d52b29bf68aad189c5f5a7ee7c01ede6
Jalpesh Vadgama <jalpesh.vadgama@gmail.com>
2019-04-27 03:37:26 +0530

    Stream demo solution comment

---
+++ RealTimeServerTime/RealTimeServerTime/Hubs/RealTimeHub.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+using System.Threading.Channels;
+
+namespace RealTimeServerTime.Hubs
+{
+    public class RealTimeHub: Hub
+    {
+        public ChannelReader<string> StartTimer()
+        {
+            var channel = Channel.CreateUnbounded<String>();
+            _ = WriteDates(channel.Writer);
+            return channel.Reader;
+        }
+
+        private async Task WriteDates(ChannelWriter<string> writer)
+        {
+            try
+            {
+                while (true)
+                {
+                    await writer.WriteAsync(DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss:fffffff"));
+                    await Task.Delay(10);
+                }
+            }
+            catch (Exception ex)
+            {
+                writer.TryComplete();
+            }
+            writer.TryComplete();
+        }
+    }
+}
---
+++ RealTimeServerTime/RealTimeServerTime/wwwroot/js/stream.js
@@ -0,0 +1,47 @@
+﻿// The following sample code uses modern ECMAScript 6 features 
+// that aren't supported in Internet Explorer 11.
+// To convert the sample for environments that do not support ECMAScript 6, 
+// such as Internet Explorer 11, use a transpiler such as 
+// Babel at http://babeljs.io/.
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+
+var connection = new signalR.HubConnectionBuilder()
+    .withUrl("/realtimehub")
+    .build();
+document.getElementById("streamButton").addEventListener("click", (event) => __awaiter(this, void 0, void 0, function* () {
+    try {
+        connection.stream("StartTimer")
+            .subscribe({
+                next: (item) => {
+                    console.log(item);
+                },
+                complete: () => {
+                    consoe.log("completed");
+                },
+                error: (err) => {
+                    console.log(Error);
+                }
+            });
+    }
+    catch (e) {
+        console.error(e.toString());
+    }
+    event.preventDefault();
+}));
+// We need an async function in order to use await, but we want this code to run immediately,
+// so we use an "immediately-executed async function"
+(() => __awaiter(this, void 0, void 0, function* () {
+    try {
+        yield connection.start();
+    }
+    catch (e) {
+        console.error(e.toString());
+    }
+}))();
